54d01ed4175c7f8619c54d7811e48a63ae7f4f94
Ryder Bright <ryderbright@gmail.com>
2016-05-02 18:25:28 -0700

    two

--- style/script.js
+++ style/script.js
@@ -40,6 +40,8 @@ window.onload = function () {
         bgReady = true;
     };
     bgImage.src = backImage[0];
+
+
     var heroImage = [
         "sprites/hero/spacey_0_one.png",
         "sprites/hero/spacey_1_one.png",
@@ -52,7 +54,8 @@ window.onload = function () {
         "sprites/hero/spacey_8_one.png",
         "sprites/hero/spacey_9_one.png",
         "sprites/hero/spacey_green.png",
-        "sprites/hero/spacey_hit.png"];
+        "sprites/hero/spacey_hit.png",
+    "sprites/hero/spacey_blessed.png"];
     //THREE
     var actionImage = [
         "sprites/hero/hero_omega1_blowup3.png",
@@ -186,6 +189,7 @@ window.onload = function () {
         "sprites/boxes/spacey_box_5_destroy_2.png",
         "sprites/boxes/spacey_box_5_destroy_3.png",
         "sprites/boxes/spacey_box_5_destroy_4.png"];
+    var bizarroImage = [];
 
 
     var MakerOne = function (x, y, speed, src, gameMove) {
@@ -206,13 +210,15 @@ window.onload = function () {
 
 
     var redShip = new MakerOne(100, 140, 500, heroImage[2], 0),
+        openScreen = new MakerOne(0, 100, 0, "sprites/spacey_open.png", 10),
         shipHit = new MakerOne(100, 140, 500, heroImage[11], 0),
+        shipHitBox = new MakerOne(100, 140, 500, heroImage[12], 0),
         smokePlume = new MakerOne(49, redShip.y, 0, actionImage[10], 10),
         smokePlumeOne = new MakerOne(37, redShip.y, 0, actionImage[10], 10),
         burner = new MakerOne(100, redShip.y, 0, actionImage[9], 10),
-        bgCloud = new MakerOne(920, 250, 0, backImage[3], 10),
+        bgCloud = new MakerOne(980, 250, 0, backImage[3], 10),
         bgCloudOne = new MakerOne(1000, 100, 0, backImage[2], 10),
-        bgCloudNine = new MakerOne(920, 175, 0, backImage[3], 10),
+        bgCloudNine = new MakerOne(1111, 175, 0, backImage[3], 10),
         redShot = new MakerOne(100, 100, 250, actionImage[5], 10),
         burnerTwo = new MakerOne(150, 0, 250, actionImage[9], 10),
         boxSmash = new MakerOne(0, 0, 100, boxImage[3], 10),
@@ -251,13 +257,13 @@ window.onload = function () {
         rockArray.push(rock);
     };
     var stoneArray = [];
-    for (i = 0; i < 5; i++) {
+    for (i = 0; i < 7; i++) {
         var stone = new MakerOne(1270 + i * 100, 50 + (Math.random() * (canvas.height - 70)), 100, blockImage[32], 10);
         stone.reset = reset;
         stoneArray.push(stone);
     };
     var goldArray = [];
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 2; i++) {
         var gold = new MakerOne(1270 + i * 100, 50 + (Math.random() * (canvas.height - 70)), 100, blockImage[81], 10);
         gold.reset = reset;
         goldArray.push(gold);
@@ -274,45 +280,53 @@ window.onload = function () {
 
     }, false);
 
+    document.addEventListener("click", function (e) {
+        keysDown[e.keyCode] = true
+
+    }, true);
+
 
     var update = function (modifier) {
 
+        if (tick > 222) {
+            for (i = 0; i < boxArray.length; i++) {
+                if (boxArray[i].gameMove === 10) {
+                    boxArray[i].x -= 6;
+                }
+                if (boxArray[i].x < canvas.width - 1270) {
+                    boxArray[i].reset();
+                }
+            }
+            for (i = 0; i < rockArray.length; i++) {
+                if (rockArray[i].gameMove === 10) {
+                    rockArray[i].x -= 4;
+                }
+                if (rockArray[i].x < -70) {
+                    rockArray[i].reset();
+                }
+            }
+            for (i = 0; i < goldArray.length; i++) {
+                if (goldArray[i].gameMove === 10) {
+                    goldArray[i].x -= 4;
+                }
+                if (goldArray[i].x < -70) {
+                    goldArray[i].reset();
+                }
 
-        for (i = 0; i < boxArray.length; i++) {
-            if (boxArray[i].gameMove === 10) {
-                boxArray[i].x -= 6;
-            }
-            if (boxArray[i].x < canvas.width - 1270) {
-                boxArray[i].reset();
-            }
-        }
-        for (i = 0; i < rockArray.length; i++) {
-            if (rockArray[i].gameMove === 10) {
-                rockArray[i].x -= 4;
-            }
-            if (rockArray[i].x < -70) {
-                rockArray[i].reset();
-            }
-        }
-        for (i = 0; i < goldArray.length; i++) {
-            if (goldArray[i].gameMove === 10) {
-                goldArray[i].x -= 4;
-            }
-            if (goldArray[i].x < -70) {
-                goldArray[i].reset();
-            }
 
-
-        }
-        for (i = 0; i < stoneArray.length; i++) {
-            if (stoneArray[i].gameMove === 10) {
-                stoneArray[i].x -= 3;
             }
-            if (stoneArray[i].x < canvas.width - 1270) {
-                stoneArray[i].reset();
+            for (i = 0; i < stoneArray.length; i++) {
+                if (stoneArray[i].gameMove === 10) {
+                    stoneArray[i].x -= 3;
+                }
+                if (stoneArray[i].x < canvas.width - 1270) {
+                    stoneArray[i].reset();
+                }
+
             }
         }
         if (37 in keysDown) {
+            redShip.src = heroImage;
             redShip.x -= redShip.speed * modifier;
         }
         if (39 in keysDown) {
@@ -332,6 +346,8 @@ window.onload = function () {
         }
         shipHit.x = redShip.x - 3;
         shipHit.y = redShip.y - 3;
+        shipHitBox.x = redShip.x - 3;
+        shipHitBox.y = redShip.y - 3;
         if (shotMove) {
             redShot.x += 21;
         }
@@ -357,11 +373,11 @@ window.onload = function () {
             bgCloudNine.x -= 2;
             smokePlume.x -= 12;
             smokePlumeOne.x -= 18;
-            afterBurn = true;
             boxSmash.y -= 0.3;
             boxSmashTwo.y -= 0.3;
+            afterBurn = true;
         }
-        
+
         if (bgCloud.x < -2000) {
             bgCloud.x = 1100;
         }
@@ -479,8 +495,8 @@ window.onload = function () {
             redVictoryOne = true;
 
         }
-         if (redDeath >= 42) {
-           gameOver = true;
+        if (redDeath >= 42) {
+            gameOver = true;
 
         }
 
@@ -494,73 +510,86 @@ window.onload = function () {
         //}
 
     };
-    if (tick = 200) {
-        tick = 0;
-    }
+
+
+    //if (tick = 200) {
+    //    tick = 0;
+    //}
 
     var drawGame = function () {
         if (bgReady) {
             context.drawImage(bgImage, 0, 0)
-        }
-
-        if (afterBurn) {
-            context.drawImage(burner.image, redShip.x - 24, redShip.y - 2)
-            
-        }
-        if (afterBurn && shotMove) {
-            context.drawImage(burnerTwo.image, redShot.x + 6, redShot.y + 5)
-        }
-        if (redShipReady) {
             context.drawImage(bgCloud.image, bgCloud.x, bgCloud.y);
             context.drawImage(bgCloudNine.image, bgCloudNine.x, bgCloudNine.y);
-            for (i = 0; i < stoneArray.length; i++) {
-                context.drawImage(stoneArray[i].image, stoneArray[i].x, stoneArray[i].y);
-            };
-            for (i = 0; i < rockArray.length; i++) {
-                context.drawImage(rockArray[i].image, rockArray[i].x, rockArray[i].y);
-            };
-            for (i = 0; i < goldArray.length; i++) {
-                context.drawImage(goldArray[i].image, goldArray[i].x, goldArray[i].y);
-                if(afterBurn){
-                     goldArray[i].src=blockImage[84];
+        }
+        if (tick < 222) {
+            context.drawImage(openScreen.image, openScreen.x, openScreen.y);
+        }
+
+        if (tick > 222) {
+            if (afterBurn) {
+                context.drawImage(burner.image, redShip.x - 24, redShip.y - 2)
+
+            }
+            if (afterBurn && shotMove) {
+                context.drawImage(burnerTwo.image, redShot.x + 6, redShot.y + 5)
+            }
+            if (redShipReady) {
+                context.drawImage(bgCloud.image, bgCloud.x, bgCloud.y);
+                context.drawImage(bgCloudNine.image, bgCloudNine.x, bgCloudNine.y);
+                for (i = 0; i < stoneArray.length; i++) {
+                    context.drawImage(stoneArray[i].image, stoneArray[i].x, stoneArray[i].y);
+                };
+                for (i = 0; i < rockArray.length; i++) {
+                    context.drawImage(rockArray[i].image, rockArray[i].x, rockArray[i].y);
+                };
+                for (i = 0; i < goldArray.length; i++) {
+                    context.drawImage(goldArray[i].image, goldArray[i].x, goldArray[i].y);
+                    if (afterBurn) {
+                        goldArray[i].src = blockImage[1];
+                    }
+                };
+                for (i = 0; i < boxArray.length; i++) {
+                    context.drawImage(boxArray[i].image, boxArray[i].x, boxArray[i].y);
+            
                 }
-            };
-            for (i = 0; i < boxArray.length; i++) {
-                context.drawImage(boxArray[i].image, boxArray[i].x, boxArray[i].y);
+                if (drawRed) {
+                    context.drawImage(redShot.image, redShot.x, redShot.y);
+                    shotMove = true;
+
+                }
+
+                context.drawImage(smokePlume.image, smokePlume.x, redShip.y + 26)
+                context.drawImage(smokePlumeOne.image, smokePlumeOne.x, redShip.y + 24)
+                context.drawImage(smokePlumeOne.image, smokePlumeOne.x + 10, redShip.y + 25)
+                context.drawImage(redShip.image, redShip.x, redShip.y);
 
             }
-            if (drawRed) {
-                context.drawImage(redShot.image, redShot.x, redShot.y);
-                shotMove = true;
 
+            if (drawSmash) {
+                context.drawImage(boxSmash.image, boxSmash.x, boxSmash.y);
+                boxSmash.x += 2;
+                boxSmash.y -= 1;
+                smashBox = true;
             }
-
-            context.drawImage(smokePlume.image, smokePlume.x, redShip.y + 26)
-            context.drawImage(smokePlumeOne.image, smokePlumeOne.x, redShip.y + 24)
-            context.drawImage(smokePlumeOne.image, smokePlumeOne.x + 10, redShip.y + 25)
-            context.drawImage(redShip.image, redShip.x, redShip.y);
-        }
-
-        if (drawSmash) {
-            context.drawImage(boxSmash.image, boxSmash.x, boxSmash.y);
-            boxSmash.x += 2;
-            boxSmash.y += 1;
-            smashBox = true;
-        }
-        if (drawSmashOne) {
-            context.drawImage(boxSmashTwo.image, boxSmashTwo.x, boxSmashTwo.y);
-            boxSmashTwo.x -= 7.1;
-            smashBoxOne = true;
-        }
-        if (drawSmashRock) {
-            context.drawImage(rockSmash.image, rockSmash.x, rockSmash.y);
-            context.drawImage(shipHit.image, shipHit.x, shipHit.y);
-            rockSmash.x -= 3;
-        }
-        if (drawSmashStone) {
-            context.drawImage(stoneSmash.image, stoneSmash.x, stoneSmash.y);
-            context.drawImage(shipHit.image, shipHit.x, shipHit.y);
-            stoneSmash.x -= 3;
+            if (drawSmashOne) {
+                context.drawImage(boxSmashTwo.image, boxSmashTwo.x, boxSmashTwo.y);
+                context.drawImage(shipHitBox.image, shipHitBox.x, shipHitBox.y);
+                boxSmashTwo.x -= 7.1;
+                smashBoxOne = true;
+            }
+            if (drawSmashRock) {
+                context.drawImage(rockSmash.image, rockSmash.x, rockSmash.y);
+                context.drawImage(shipHit.image, shipHit.x, shipHit.y);
+                rockSmash.x -= 2;
+                rockSmash.y += 1;
+            }
+            if (drawSmashStone) {
+                context.drawImage(stoneSmash.image, stoneSmash.x, stoneSmash.y);
+                context.drawImage(shipHit.image, shipHit.x, shipHit.y);
+                stoneSmash.x -= 2;
+                stoneSmash.y += 1;
+            }
         }
 
 
@@ -571,8 +600,8 @@ window.onload = function () {
 
         update(delta / 1000);
         drawGame();
-        ++tick;
         then = now;
+        ++tick
         requestAnimationFrame(tickTock);
     };
 
